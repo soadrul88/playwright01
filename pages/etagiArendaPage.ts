abd7861410a28abd8d7c88c03abdbd0a155f4dac
Iliya <soadrul@mail.ru>
2025-09-15 17:15:16 +0500

    push

--- pages/etagiArendaPage.ts
+++ pages/etagiArendaPage.ts
@@ -3,32 +3,41 @@ import { Locator, Page } from '@playwright/test';
 export class EtagiArendaPage {
   page: Page;
   path: string;
-  //filterClick: Locator;
+  filterClick: Locator;
   languageClick: Locator;
   apartmentsClick: Locator;
   bedroomsClick: Locator;
-  //priceClick: Locator;
-  //priceInputFrom: Locator;
-  //priceInputTo: Locator;
+  bedroomsClickArea: Locator;
+  priceClick: Locator;
+  priceInputFrom: Locator;
+  priceInputTo: Locator;
 
   constructor(page: Page, path: string) {
     this.page = page;
     this.path = path;
     this.languageClick = this.page.getByTestId('modal_button_close');
     this.apartmentsClick = this.page.getByRole('button', { name: 'Квартиры' });
-    this.bedroomsClick = this.page.locator('span').filter({ hasText: /^3$/ }).first();
+    this.bedroomsClickArea = this.page.locator('span').filter({ hasText: /^3$/ }).first();
+    this.bedroomsClick = this.page.getByRole('button', { name: 'Комнатность' });
+    this.priceClick = this.page.getByRole('button', { name: 'Комнатность' });
+    this.priceInputFrom = this.page.getByRole('button', { name: 'Комнатность' });
+    this.priceInputTo = this.page.getByRole('button', { name: 'Комнатность' });
+    this.filterClick = this.page.getByRole('button', { name: 'Фильтры' });
   }
   async language() {
     await this.languageClick.click();
   }
-  //async filter() {
-  //  await this.filterClick.click();
-  //}
+  async filter() {
+    await this.filterClick.click();
+    await this.page.waitForTimeout(1500);
+  }
   async apartmentsPick() {
     await this.apartmentsClick.click();
-    await this.page.waitForTimeout(1500);
   }
   async bedroomsPick() {
     await this.bedroomsClick.click();
+  }
+  async bedroomsPickArea() {
+    await this.bedroomsClickArea.click();
   }
 }
--- tests/E-soft/etagiArenda.spec.ts
+++ tests/E-soft/etagiArenda.spec.ts
@@ -2,6 +2,8 @@ import { test } from '../../fixtures';
 
 test('Проверка фильтра', async ({ etagiArendaPage }) => {
   await etagiArendaPage.language();
-  await etagiArendaPage.apartmentsPick();
+  //await etagiArendaPage.apartmentsPick();
   await etagiArendaPage.bedroomsPick();
+  await etagiArendaPage.bedroomsPickArea();
+  await etagiArendaPage.page.waitForTimeout(1500);
 });
