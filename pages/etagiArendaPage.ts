b7c90c91827717c85abbccbbc6de4d41ed51d074
Iliya <soadrul@mail.ru>
2025-09-15 16:45:20 +0500

    push

--- pages/etagiArendaPage.ts
+++ pages/etagiArendaPage.ts
@@ -3,13 +3,13 @@ import { Locator, Page } from '@playwright/test';
 export class EtagiArendaPage {
   page: Page;
   path: string;
-  filterClick: Locator;
+  //filterClick: Locator;
   languageClick: Locator;
   apartmentsClick: Locator;
   bedroomsClick: Locator;
-  priceClick: Locator;
-  priceInputFrom: Locator;
-  priceInputTo: Locator;
+  //priceClick: Locator;
+  //priceInputFrom: Locator;
+  //priceInputTo: Locator;
 
   constructor(page: Page, path: string) {
     this.page = page;
@@ -17,21 +17,16 @@ export class EtagiArendaPage {
     this.languageClick = this.page.getByTestId('modal_button_close');
     this.apartmentsClick = this.page.getByRole('button', { name: 'Квартиры' });
     this.bedroomsClick = this.page.locator('span').filter({ hasText: /^3$/ }).first();
-    this.bedroomsClick = this.page.getByRole('button', { name: 'Комнатность' });
-    this.priceClick = this.page.getByRole('button', { name: 'Комнатность' });
-    this.priceInputFrom = this.page.getByRole('button', { name: 'Комнатность' });
-    this.priceInputTo = this.page.getByRole('button', { name: 'Комнатность' });
-    this.filterClick = this.page.getByRole('button', { name: 'Фильтры' });
   }
   async language() {
     await this.languageClick.click();
   }
-  async filter() {
-    await this.filterClick.click();
-    await this.page.waitForTimeout(1500);
-  }
+  //async filter() {
+  //  await this.filterClick.click();
+  //}
   async apartmentsPick() {
     await this.apartmentsClick.click();
+    await this.page.waitForTimeout(1500);
   }
   async bedroomsPick() {
     await this.bedroomsClick.click();
