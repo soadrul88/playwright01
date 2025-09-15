af1988aa05690813d2c5cf5582b992d116f5685c
Iliya <soadrul@mail.ru>
2025-09-15 17:37:50 +0500

    push

--- pages/etagiArendaPage.ts
+++ pages/etagiArendaPage.ts
@@ -3,7 +3,7 @@ import { Locator, Page } from '@playwright/test';
 export class EtagiArendaPage {
   page: Page;
   path: string;
-  filterClick: Locator;
+  //filterClick: Locator;
   languageClick: Locator;
   apartmentsClick: Locator;
   bedroomsClick: Locator;
@@ -19,18 +19,18 @@ export class EtagiArendaPage {
     this.apartmentsClick = this.page.getByRole('button', { name: 'Квартиры' });
     this.bedroomsClickArea = this.page.locator('span').filter({ hasText: /^3$/ }).first();
     this.bedroomsClick = this.page.getByRole('button', { name: 'Комнатность' });
-    this.priceClick = this.page.getByRole('button', { name: 'Комнатность' });
-    this.priceInputFrom = this.page.getByRole('button', { name: 'Комнатность' });
-    this.priceInputTo = this.page.getByRole('button', { name: 'Комнатность' });
-    this.filterClick = this.page.getByRole('button', { name: 'Фильтры' });
+    this.priceClick = this.page.getByRole('button', { name: 'Стоимость, ₽' });
+    this.priceInputFrom = this.page.locator('.hH5W3').first();
+    this.priceInputTo = this.page.locator('span').filter({ hasText: '₽' }).nth(1);
+    //this.filterClick = this.page.getByRole('button', { name: 'Фильтры' });
   }
   async language() {
     await this.languageClick.click();
   }
-  async filter() {
-    await this.filterClick.click();
-    await this.page.waitForTimeout(1500);
-  }
+  //async filter() {
+  //  await this.filterClick.click();
+  //  await this.page.waitForTimeout(1500);
+  //}
   async apartmentsPick() {
     await this.apartmentsClick.click();
   }
@@ -39,5 +39,15 @@ export class EtagiArendaPage {
   }
   async bedroomsPickArea() {
     await this.bedroomsClickArea.click();
+  }
+  async pricePick() {
+    await this.priceClick.click();
+    await this.page.waitForTimeout(1500);
+  }
+  async priceFrom(priceFrom: string) {
+    await this.priceInputFrom.fill(priceFrom);
+  }
+  async priceTo(priceTo: string) {
+    await this.priceInputTo.fill(priceTo);
   }
 }
--- tests/E-soft/etagiArenda.spec.ts
+++ tests/E-soft/etagiArenda.spec.ts
@@ -1,9 +1,16 @@
 import { test } from '../../fixtures';
 
 test('Проверка фильтра', async ({ etagiArendaPage }) => {
+  const data = {
+    priceFrom: '10000',
+    priceTo: '30000',
+  };
   await etagiArendaPage.language();
-  //await etagiArendaPage.apartmentsPick();
+  await etagiArendaPage.apartmentsPick();
   await etagiArendaPage.bedroomsPick();
   await etagiArendaPage.bedroomsPickArea();
   await etagiArendaPage.page.waitForTimeout(1500);
+  await etagiArendaPage.pricePick();
+  await etagiArendaPage.priceFrom(data.priceFrom);
+  await etagiArendaPage.priceTo(data.priceTo);
 });
